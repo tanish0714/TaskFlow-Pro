2190930d7cd53fbcb27911d4169cbbf2e4b0ddb0
tanish0714 <tanish.230101144@iiitbh.ac.in>
2026-05-16 16:56:11 +0530

    Fixing Production ready routing

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
+
   withCredentials: true,
 });
 
 export default API;
+
+
+
