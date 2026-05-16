faa5541b1f71cb3ea5b820cd11cf653c92f158a8
tanish0714 <tanish.230101144@iiitbh.ac.in>
2026-05-16 18:02:46 +0530

    Fixing JWT generation

--- jwt/generateToken.js
+++ jwt/generateToken.js
@@ -1,14 +1,35 @@
-import jwt from 'jsonwebtoken'
-// created for validation jiske pass token hoga whi chat kr payega
-const createTokenAndSaveCookie=(userId,res)=>{
-    const token = jwt.sign({userId},process.env.JWT_TOKEN,{
-        expiresIn:"10d"
-    })
-    res.cookie('jwt',token,{
-        httpOnly:true, // saves from accessive attacks
-        secure:true,  
-        sameSite:"none" // stricts same sites
-    })
-    
-}
-export default createTokenAndSaveCookie
+import jwt from "jsonwebtoken";
+
+const createTokenAndSaveCookie = (
+  userId,
+  res
+) => {
+
+  const token = jwt.sign(
+    { userId },
+    process.env.JWT_TOKEN,
+    {
+      expiresIn: "10d",
+    }
+  );
+
+  res.cookie("jwt", token, {
+
+    httpOnly: true,
+
+    secure:
+      process.env.NODE_ENV ===
+      "production",
+
+    sameSite:
+      process.env.NODE_ENV ===
+      "production"
+        ? "none"
+        : "lax",
+
+    maxAge:
+      10 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export default createTokenAndSaveCookie;
